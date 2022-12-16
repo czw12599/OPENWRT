dcf429e60bbab42ce49445455d1476f98f441f55
czw12599 <80137943+czw12599@users.noreply.github.com>
2022-12-16 15:36:33 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -3,78 +3,61 @@
 # DIY扩展二合一了，在此处可以增加插件
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
-# 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
-# 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
-# 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
 
+export IPv4_ipaddr="192.168.5.3"           # 修改openwrt后台地址(填0为关闭)
+export Netmask_netm="255.255.255.0"        # IPv4 子网掩码（默认：255.255.255.0）(填0为关闭)
+export Op_name="OpenWrt"                   # 修改主机名称为OpenWrt-123(填0为关闭)
 
-cat >$NETIP <<-EOF
-uci set network.lan.ipaddr='192.168.5.3'                      # IPv4 地址(openwrt后台地址)
-uci set network.lan.netmask='255.255.255.0'                   # IPv4 子网掩码
-uci set network.lan.delegate='0'                              # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请注释或者删除这个)
-#uci set dhcp.@dnsmasq[0].filter_aaaa='1'                     # 禁止解析 IPv6 DNS记录(若用IPV6请注释或者删除这个)
-#uci set dhcp.lan.ignore='1'                                  # 关闭DHCP功能（去掉uci前面的#生效）
-uci set system.@system[0].hostname='OpenWrt'                  # 修改主机名称为OpenWrt
-uci set ttyd.@ttyd[0].command='/bin/login -f root'           # 设置ttyd免帐号登录（去掉uci前面的#生效）
-EOF
+export Router_gateway="192.168.5.3"         # 旁路由设置 IPv4 网关(填0为关闭)
+export Lan_DNS="114.114.114.114 223.5.5.5"  # 旁路由设置 DNS(多个DNS要用空格分开)(填0为关闭)
+export IPv4_Broadcast="192.168.5.255"       # 设置 IPv4 广播(填0为关闭)
+export Close_DHCP="0"                       # 旁路由关闭DHCP功能(1为开,0为关闭)
+export Delete_Bridge="0"                    # 旁路由去掉桥接模式(1为开,0为关闭)
+export ttyd_Nopassword="0"                  # 设置ttyd免密登录(1为开,0为关闭)
 
+export OpenClash_branch="0"            # OpenClash代码选择分支（master 或 dev）(填0为不需要此插件)
+export OpenClash_Core="0"                   # 编译固件增加OpenClash时,把核心下载好,核心为3MB左右大小(1为开,0为关闭)
+export Required_Topic="argon"               # 将bootstrap替换您需要的主题为必选主题,名称必须写对,源码内必须有该主题(填0为关闭)
+export Default_Theme="argon"                # 多主题时,选择某主题为默认第一主题 (填写主题名称,填0为关闭)
+export Delete_NotRequired="1"               # 个别机型内一堆其他机型固件,删除其他机型的,只保留当前主机型固件(1为开,0为关闭)
+export Kernel_Patchver="6.1"                # 更换内核版本,前提是您编译的机型源码内保证有其他内核存在(0为关闭,写上其他表示替换)
+export Confidentiality_free="1"             # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为开,0为关闭)
+export Remove_Firewall="0"                  # 删除DNS强制重定向53端口防火墙规则(1为开,0为关闭)
+export Cancel_running="1"                   # 取消路由器每天跑分任务
+export AdGuardHome_Core="1"                 # 编译固件增加AdGuardHome时,把核心下载好,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为开,0为关闭)
 
-# 设置 argon 为编译必选主题(可自行修改您要的,主题名称必须对,源码内必须有该主题)
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+export Personal_Signature="0"  # 个性签名,你想写啥就写啥，(填0为关闭)
 
-
-# 编译多主题时,设置某主题成默认主题（危险操作,你要确定您这里改的主题的名字准确,比如[argon]和肯定编译了该主题,要不然进不了后台）
-#sed -i "/exit 0/i\uci set luci.main.mediaurlbase='/luci-static/neobird2' && uci commit luci" "$BASE_PATH/etc/rc.local"
-
-
-# 增加个性名字 ${Author} 默认为你的github帐号,修改时候把 ${Author} 替换成你要的
-sed -i "s/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
-
-
-# 设置首次登录后台密码为空（进入openwrt后自行修改密码）
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ_PATH
-
-
-# 删除默认防火墙
-sed -i '/to-ports 53/d' $ZZZ_PATH
-
-
-# 取消路由器每天跑分任务
-sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "$BASE_PATH/etc/rc.local"
-
-
-# x86机型,默认内核5.15，修改内核为5.10（源码时时变,自行根据target/linux/x86/Makefile文件修改）
-sed -i 's/PATCHVER:=5.15/PATCHVER:=6.0/g' target/linux/x86/Makefile
-
-
-# K3专用，编译K3的时候只会出K3固件（其他机型也适宜,把phicomm_k3替换一下，名字要绝对正确才行）
-#sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
-
-
-# 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
-cat >$DELETE <<-EOF
-EOF
 
 
 # 修改插件名字
-sed -i 's/"aMule设置"/"电驴下载"/g' `grep "aMule设置" -rl ./`
-sed -i 's/"网络存储"/"NAS"/g' `grep "网络存储" -rl ./`
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
-sed -i 's/"实时流量监测"/"流量"/g' `grep "实时流量监测" -rl ./`
-sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
-sed -i 's/"TTYD 终端"/"命令窗"/g' `grep "TTYD 终端" -rl ./`
-sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
-sed -i 's/"Web 管理"/"Web"/g' `grep "Web 管理" -rl ./`
-sed -i 's/"管理权"/"改密码"/g' `grep "管理权" -rl ./`
-sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./`
+sed -i 's?"aMule设置"?"电驴下载"?g' `grep -Eo '"aMule设置"' -rl ./`
+sed -i 's?"网络存储"?"NAS"?g' `grep -Eo '"网络存储"' -rl ./`
+sed -i 's?"Turbo ACC 网络加速"?"网络加速"?g' `grep -Eo '"Turbo ACC 网络加速"' -rl ./`
+sed -i 's?"实时流量监测"?"流量"?g' `grep -Eo '"实时流量监测"' -rl ./`
+sed -i 's?"KMS 服务器"?"KMS激活"?g' `grep -Eo '"KMS 服务器"' -rl ./`
+sed -i 's?"TTYD 终端"?"TTYD"?g' `grep -Eo '"TTYD 终端"' -rl ./`
+sed -i 's?"USB 打印服务器"?"打印服务"?g' `grep -Eo '"USB 打印服务器"' -rl ./`
+sed -i 's?"Web 管理"?"Web管理"?g' `grep -Eo '"Web 管理"' -rl ./`
+sed -i 's?"管理权"?"改密码"?g' `grep -Eo '"管理权"' -rl ./`
+sed -i 's?"带宽监控"?"监控"?g' `grep -Eo '"带宽监控"' -rl ./`
 
+# 拉取旧版OpenClash固件
+# svn co https://github.com/shidahuilang/OpenClash package/luci-app-xiaolaoshu
 
-# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间（根据编译机型变化,自行调整需要删除的固件名称）
-cat >${GITHUB_WORKSPACE}/Clear <<-EOF
-rm -rf config.buildinfo
-rm -rf feeds.buildinfo
-rm -rf sha256sums
-rm -rf version.buildinfo
-rm -rf manifest
+# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
+cat >"$CLEAR_PATH" <<-EOF
+packages
+config.buildinfo
+feeds.buildinfo
+openwrt-x86-64-generic-kernel.bin
+openwrt-x86-64-generic.manifest
+openwrt-x86-64-generic-squashfs-rootfs.img.gz
+sha256sums
+version.buildinfo
+EOF
+
+# 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
+cat >>$DELETE <<-EOF
 EOF
